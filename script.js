22aa796260ace2b29de7b30fedc21f0c56a95075
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-07-25 23:58:49 +0530

    weather error condition added

--- script.js
+++ script.js
@@ -5,15 +5,22 @@ let weatherIcon = document.getElementById("weather-icon")
 let searchInput = document.getElementById("search-input");
 let searchBtn = document.getElementById("search-btn");
 
+
+function resetWeather(){
+   temperature.innerText = "0°";
+   cityName.innerText = "--";
+   weatherCondition.innerText = "--";
+   weatherIcon.src = "assets/weather/default.png";
+
+}
+
+
 function searchWeather(){
    const city = searchInput.value.trim();
 
    if(city === ""){
       alert("Please enter the city");
-      temperature.innerText = "0°";
-      cityName.innerText = "--";
-      weatherCondition.innerText = "--";
-      weatherIcon.src = "assets/weather/default.png";
+      resetWeather();
       return;
    }
 
@@ -30,9 +37,15 @@ searchInput.addEventListener("keydown",(event) => {
 
 async function getWeather(city){
 
-    try{
-    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=82c6260c88cb444aa5604743262507&q=${city}`)
-    const data = await response.json();
+   try{
+   const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=82c6260c88cb444aa5604743262507&q=${city}`);
+   
+   const data = await response.json();
+   if(data.error){
+      alert(data.error.message);
+      resetWeather();
+      return;
+   }
     temperature.innerText = data.current.temp_c+"°";
     cityName.innerText = data.location.name + "," + data.location.country;
     weatherCondition.innerText = data.current.condition.text;
@@ -42,6 +55,8 @@ async function getWeather(city){
 
     catch(error){
       alert("Please enter a valid city");
+      
+      console.log(error);
     }
 }
 
