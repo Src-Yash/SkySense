59ae42b47ecf4deecad01e9dbf6d6d79719afe04
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-07-25 07:12:43 +0530

    modified the changes

--- script.js
+++ script.js
@@ -5,6 +5,24 @@ let weatherIcon = document.getElementById("weather-icon")
 let searchInput = document.getElementById("search-input");
 let searchBtn = document.getElementById("search-btn");
 
+function searchWeather(){
+   const city = searchInput.ariaValueMax.trim();
+
+   if(city === ""){
+      alert("Please enter the city");
+      return;
+   }
+
+   getWeather(city);
+}
+searchBtn.addEventListener("click",searchWeather);
+
+searchInput.addEventListener("keydown",(event) => {
+   if(event.key==="Enter");
+     { 
+      searchWeather();
+     }
+});
 
 async function getWeather(city){
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=82c6260c88cb444aa5604743262507&q=${city}`)
