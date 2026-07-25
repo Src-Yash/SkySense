0beff9c847cc2165c6d4273f645c3299522c8ed1
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-07-25 06:35:43 +0530

    successfully learned how to call a API

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-let searchInput=document.querySelector("search-input");
-let weatherIcon=document.querySelector("weather-image");
+async function getWeather(){
+    const response = await fetch("https://api.weatherapi.com/v1/current.json?key=82c6260c88cb444aa5604743262507&q= Ghaziabad")
+    const data = await response.json();
+    console.log(data.current.temp_c);
+    console.log(data.current.temp_f);
+    console.log(data.current.wind_kph);
+    console.log(data.location.country);
+    console.log(data);
+}
 
+getWeather();
