1cf738178ce21dee85b7d0069a71e79a208b4242
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-08-02 04:08:17 +0530

    AI integration of Gemini Google

--- script.js
+++ script.js
@@ -33,8 +33,9 @@ let aboutPage=document.getElementById("aboutPage")
 
 let forecastBtn = document.querySelector(".forecastBtn");
 let aboutBtn = document.querySelector(".aboutBtn")
-
-
+let dailyBrief = document.getElementById("dailyBrief")
+let weatherScore = document.getElementById("weatherScore");
+let weatherStatus = document.getElementById("weatherStatus");
 
 function showForecast(){
    forecastPage.style.display="block";
@@ -76,6 +77,104 @@ farhenElement.addEventListener("click", () => {
    //  farhenElement.style.background="#FFC5AA";
     updateTemperature();
 });
+
+
+// section ABOUT YOU Functions
+
+
+function generateDailyBrief(data){
+   const temp = data.current.temp_c;
+   const humidity = data.current.humidity;
+   const uv = data.current.uv;
+   const wind = data.current.wind_kph;
+   const condition = data.current.condition.text;
+   let message = "";
+   if(temp>=38){
+      message+="It's going to be extremly hot today, stay hydrated.<br>"
+   }
+   else if(temp>=30){
+      message+="It's a warm day today.<br>";
+   }
+  
+   else{
+      message+="the weather is pleasant today.<br>";
+   }
+   if(uv>=8){
+      message+="UV is ver high. Wear sunscreen.<br>"
+   }
+   if(humidity>80){
+      message+="Humidity is high. Wear sunscreen. <br>";
+   }
+   if(wind>35){
+      message+="Strong winds are expected.<br>";
+   }
+   if(condition.includes("Rain")){
+      message+="Carry an umbrella today.<br>";
+   }
+
+   dailyBrief.innerHTML=message;
+
+
+};
+
+function calculateWeatherScore(data){
+   let score=100;
+   const temp = data.current.temp_c;
+   
+      if(temp > 38)
+         score -= 25;
+      else if(temp > 34)
+         score -= 15;
+      else if(temp < 10)
+         score -= 20;
+   
+   const humidity = data.current.humidity;
+
+      if(humidity>85){
+         score-=15;
+      }
+
+   const uv=data.current.uv; 
+      if(uv>8){
+         score-=15;
+      }
+   const wind = data.current.wind_kph;
+   
+   if(wind > 40)
+       score -= 20;
+
+   const condition = data.current.condition.text;
+
+   if(condition.includes("Rain"))
+       score -= 15;
+
+   if(score<0){
+      score=0;
+   }
+
+   let status = "";
+
+if(score >= 90)
+    status = "⭐⭐⭐⭐⭐ Excellent";
+
+else if(score >= 75)
+    status = "⭐⭐⭐⭐ Very Good";
+
+else if(score >= 60)
+    status = "⭐⭐⭐ Fair";
+
+else if(score >= 40)
+    status = "⭐⭐ Poor";
+
+else
+    status = "⭐ Stay Indoors";
+
+
+weatherScore.innerText = score + "/100";
+weatherStatus.innerText = status;
+
+      
+}
 
 
 function resetWeather(){
@@ -329,6 +428,8 @@ async function getWeather(city){
       
    sunsetElement.innerText=data.forecast.forecastday[0].astro.sunset;
 
+   generateDailyBrief(data);
+   calculateWeatherScore(data);
 
    // }
  
@@ -345,6 +446,10 @@ async function getWeather(city){
 //    const image = await fetch(`https://apiunsplash.com/search/photos?query=${city}&client_id=`)
    
 // }
+
+async function getAIRecommendations(data){
+   const ai=await fetch(`https://generativelanguage.googleapis.com`)
+}
 
 
 getWeather("New Delhi");
