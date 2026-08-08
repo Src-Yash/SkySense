4212225f5ec15028c100a27e31ad9b75c3197a90
Yash Srivastava <yashsrivastava.intent@gmail.com>
2026-08-08 20:09:06 +0530

    making the server to deploy

--- backend/server.js
+++ backend/server.js
@@ -148,6 +148,8 @@ app.post("/recommendations",async (req,res) => {
   }
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
 });
